58eddc1fa9c49849603d96d594d311f5c322e06d
Brady Sauter <07scaping@gmail.com>
2020-08-04 20:56:44 -0400

    copies from input to output successfully

--- plecoToAnki.cpp
+++ plecoToAnki.cpp
@@ -9,8 +9,19 @@ int main() {
     string outputFile = "ankiImport.txt";
     remove(outputFile.c_str());    
     ofstream output(outputFile);
+    string buffer;
 
-
+    while (getline (input, buffer, '\t')) {
+        // simplified characters
+        output << buffer << '\t';
+        // pinyin
+        getline (input, buffer, '\t');
+        output << buffer << '\t';
+        // definition
+        getline (input, buffer);
+        output << buffer << endl;
+    }
+    
     input.close();
     output.close();
     return 0;
