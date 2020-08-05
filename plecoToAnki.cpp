efd98fab26c437e0cda8574f28ab911fd466ea7d
Brady Sauter <07scaping@gmail.com>
2020-08-04 21:43:04 -0400

    moved code into a class

--- plecoToAnki.cpp
+++ plecoToAnki.cpp
@@ -2,27 +2,72 @@
 #include <fstream>
 #include <string>
 #include <cstdio>
+#include <vector>
+#include <unordered_set>
 using namespace std;
 
-int main() {
-    ifstream input("plecoExport.txt");
-    string outputFile = "ankiImport.txt";
-    remove(outputFile.c_str());    
-    ofstream output(outputFile);
+class toAnki {
+  private:
+    unordered_set<string> partsOfSpeech;
+    ifstream input;
+    ofstream output;
     string buffer;
 
-    while (getline (input, buffer, '\t')) {
-        // simplified characters
-        output << buffer << '\t';
-        // pinyin
-        getline (input, buffer, '\t');
-        output << buffer << '\t';
-        // definition
-        getline (input, buffer);
-        output << buffer << endl;
+  public:
+    toAnki(string inputFile = "plecoExport.txt", 
+                string outputFile = "ankiImport.txt") {
+
+        createPartsOfSpeechSet();
+        input.open(inputFile);
+        remove(outputFile.c_str());    
+        output.open(outputFile);
     }
+
+    ~toAnki() {
+        input.close();
+        output.close();
+    }
+
+    bool plecoToAnki() {
+        while (getline (input, buffer, '\t')) {
+            // simplified characters
+            output << buffer << '\t';
+            // pinyin
+            getline (input, buffer, '\t');
+            output << buffer << '\t';
+            // definition
+            getline (input, buffer);
+            output << buffer << endl;
+
+
+            //output << "<ol><li>";
+            //for (int i = 0; i < buffer.size(); i++) {
+
+            //}
+            //output << "</ol>";
+        }
+    }
+
+    void createPartsOfSpeechSet() {
+        vector<string> partOfSpeechVector = {"noun", "verb", "adjective", "adverb", "pronoun",
+                                            "preposition", "conjunction","interjection", 
+                                            "numeral", "article", "determiner"};
+        for (auto i : partOfSpeechVector) {
+            partsOfSpeech.insert(i);
+        }
+    }
+
+    bool isPartOfSpeech(string s) {
+        if (partsOfSpeech.find(s) != partsOfSpeech.end()) {
+            return true;
+        }
+        return false;
+    }
+};
+
+int main() {
+    toAnki myConverter("plecoExport.txt", "ankiImport.txt");
+    myConverter.plecoToAnki();
     
-    input.close();
-    output.close();
     return 0;
 }
